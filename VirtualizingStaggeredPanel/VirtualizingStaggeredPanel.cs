4ff8d8dbc3cf436ebdc11cb33932be80298d35f6
MikiraSora <mikirasora0409@126.com>
2021-12-09 02:09:34 +0800

    add comments and optimze code

--- VirtualizingStaggeredPanel/VirtualizingStaggeredPanel.cs
+++ VirtualizingStaggeredPanel/VirtualizingStaggeredPanel.cs
@@ -174,9 +174,11 @@ namespace MikiraSora.VirtualizingStaggeredPanel
 
         private void CheckItemSourceChanged()
         {
+            //todo 这玩意能优化
+
             foreach (var newAppended in ItemSource.Reverse().TakeWhile(x => !x.__HasInserted).Reverse())
             {
-                //todo 处理新增的
+                //处理新增的
                 newAppended.__HasInserted = false;
                 var minOne = containers.OrderBy(x => x.MaxOffsetY).FirstOrDefault();
                 if (minOne != null)
@@ -189,7 +191,7 @@ namespace MikiraSora.VirtualizingStaggeredPanel
             foreach (var newPrepended in ItemSource.TakeWhile(x => !x.__HasInserted))
             {
                 newPrepended.__HasInserted = false;
-                //todo 处理前面加的
+                //处理前面加的
                 var maxOne = containers.OrderByDescending(x => x.MinOffsetY).FirstOrDefault();
                 if (maxOne != null)
                 {
@@ -212,30 +214,47 @@ namespace MikiraSora.VirtualizingStaggeredPanel
 
         protected override Size MeasureOverride(Size availableSize)
         {
+            /**
+             
+              通常虚拟化的MeasureOverride()的职责:
+              * 检查InternalChildren那些ui元素是否还能显示，不能就从前者删了
+              * 负责筹算ItemSource集合中哪些Item是需要显示,然后将他们通过ItemContainerGenerator转换成ui元素塞进InternalChildren
+              * 计算显示范围(ViewPort)和理论总长(Extend)
+              * 通过ViewPort/Extend去检查并更新滚动条
+              
+             */
+
+            //step0:宽度变化检查,如果宽度变动且导致列数不对了，那就重新生成新的列,并释放所有的Item
             var isRebuild = CheckAndReBuildContainers(availableSize);
 
             UIElementCollection children = this.InternalChildren;
             IItemContainerGenerator generator = this.ItemContainerGenerator;
 
+            //step1:检查有没有新的Item添加到后面/前面，再把它们塞到对应的列中
             CheckItemSourceChanged();
+
             var minOffsetY = containers.OrderBy(x => x.MinOffsetY).FirstOrDefault()?.MinOffsetY ?? 0;
             var maxOffsetY = containers.OrderByDescending(x => x.MaxOffsetY).FirstOrDefault()?.MaxOffsetY ?? 0;
-            var colCalcResult = containers.Select((x, i) => (GetVisibleItemParams(ref availableSize, x, minOffsetY, maxOffsetY), x, i)).ToArray();
 
+            //step2:计算出每个列，能够显示的Item集合并按照ItemSource的位置排序
+            var colCalcResult = containers.Select((x, i) => (GetVisibleItemParams(availableSize, x, minOffsetY), x, i)).ToArray();
             var visibleParams = colCalcResult
-                .SelectMany(x => x.Item1.paramList.Select(y => (y, x.x, x.i)))
+                .SelectMany(x => x.Item1.Select(y => (y, x.x, x.i)))
                 .OrderBy(x => x.y.__ItemIndex)
                 .ToArray();
 
             var prevItemIndex = int.MinValue;
             IDisposable currentGeneratorStatus = default;
 
+            //暂时不判断存留,直接清空InternalChildren，能显示的后面再加
             RemoveInternalChildRange(0, children.Count);
 
             elementMap.Clear();
 
+            //step3:将计算好的可显示Item依次生成子空间，并塞进InternalChildren
             foreach ((var param, var col, var i) in visibleParams)
             {
+                //因为可能存在超长Item导致整个visibleParams并非按1等差递增数列，可以通过此特性对相邻的Item可以直接一次StartAt
                 if (param.__ItemIndex - prevItemIndex != 1)
                 {
                     //not next. (re)generate UIElement generator.
@@ -250,39 +269,48 @@ namespace MikiraSora.VirtualizingStaggeredPanel
                 AddInternalChild(childElement);
 
                 if (newlyRealized)
-                {
                     generator.PrepareItemContainer(childElement);
-                }
 
+                //测量一下
                 childElement.Measure(availableSize);
                 prevItemIndex = param.__ItemIndex;
             }
 
             currentGeneratorStatus?.Dispose();
 
+            //step4:更新一下滚动条
             UpdateScrollInfo(availableSize);//availableSize更新后，更新滚动条
-            Debug.WriteLine($"called MeasureOverride() {InternalChildren.Count} {availableSize}");
+            //Debug.WriteLine($"called MeasureOverride() {InternalChildren.Count} {availableSize}");
 
             var height = maxOffsetY - minOffsetY;
             var size = availableSize;
             size.Height = double.IsInfinity(availableSize.Height) ? height : availableSize.Height;
 
+            //返回可视大小(ViewPort)
             return size;
         }
 
         private void UpdateScrollInfo(Size availableSize)
         {
             var extent = CalculateExtent(availableSize);
+            var isVaild = true;
+
             if (extent != this.extent)
             {
+                //更新理论总大小
                 this.extent = extent;
-                ScrollOwner?.InvalidateScrollInfo();
+                isVaild = false;
             }
+
             if (availableSize != viewPort)
             {
+                //更新可视大小
                 viewPort = availableSize;
-                ScrollOwner?.InvalidateScrollInfo();
+                isVaild = false;
             }
+
+            if (!isVaild)
+                ScrollOwner?.InvalidateScrollInfo();
         }
 
         private Size CalculateExtent(Size availableSize)
@@ -290,49 +318,71 @@ namespace MikiraSora.VirtualizingStaggeredPanel
             return new(availableSize.Width, containers.OrderByDescending(x => x.DesiredHeight).FirstOrDefault()?.DesiredHeight ?? 0);
         }
 
-        private (List<IVirtualGridFlowPanelItemParam> paramList, double calcColMinY, double calcColMaxY) GetVisibleItemParams(ref Size availableSize, ItemLinearColumnContainer col, double minOffsetY, double maxOffsetY)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="availableSize"></param>
+        /// <param name="col"></param>
+        /// <param name="minOffsetY"></param>
+        /// <param name="maxOffsetY"></param>
+        /// <returns>在可视范围内的Item集合</returns>
+        private List<IVirtualGridFlowPanelItemParam> GetVisibleItemParams(Size availableSize, ItemLinearColumnContainer col, double minOffsetY)
         {
             var paramList = new List<IVirtualGridFlowPanelItemParam>();
             var calcColMinY = double.MaxValue;
             var calcColMaxY = double.MinValue;
             var width = GridItemWidth;
+            var baseY = col.MinOffsetY - minOffsetY;
 
-            //build view bound.
+            //钦定当前有效的可视范围大小.
             var bound = new Rect(0, VerticalOffset, int.MaxValue, availableSize.Height);
             var y = 0d;
 
             for (int i = 0; i < col.Children.Count; i++)
             {
                 var param = col.Children[i];
-                var bottom = col.MinOffsetY - minOffsetY + y;
+                var bottom = baseY + y;
                 var height = width / param.AspectRatio;
 
+                //计算出Item对应的位置和大小
                 var paramRect = new Rect(0, bottom, width, height);
+
+                //包围检查
                 if (bound.IntersectsWith(paramRect))
                 {
                     calcColMinY = Math.Min(calcColMinY, y);
                     paramList.Add(param);
                     calcColMaxY = Math.Max(calcColMaxY, y + height);
                 }
+                else if (paramList.Count > 0) //优化一下，已经超出了可视范围那后面就没必要检查了
+                    break;
+
                 y += height;
             }
 
-            return (paramList, calcColMinY, calcColMaxY);
+            return paramList;
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
-            //获取最低的偏移量，用到后面的全局y轴计算。
+            /**
+             
+              通常虚拟化的ArrangeOverride()的职责:
+              * 遍历InternalChildren，钦定各个子控件的位置和大小
+              * 通过ViewPort/Extend去检查并更新滚动条
+              
+             */
+
+            //step0:获取最低的偏移量，用到后面的全局y轴计算。
             var minBaseOffsetY = containers.OrderBy(x => x.MinOffsetY).FirstOrDefault()?.MinOffsetY ?? 0;
             var width = GridItemWidth;
-            Debug.WriteLine($"called ArrangeOverride() minBaseOffsetY:{minBaseOffsetY}");
+            //Debug.WriteLine($"called ArrangeOverride() minBaseOffsetY:{minBaseOffsetY}");
 
             foreach (UIElement uiElement in InternalChildren)
             {
                 if (!(elementMap.TryGetValue(uiElement, out var zz) && zz is (var param, var colIndex) && containers.ElementAtOrDefault(colIndex) is ItemLinearColumnContainer col))
                     continue;
 
-                //计算x
                 var x = colIndex * width;
 
                 //step 1:计算出此param在此col的相对位置,得到y1
@@ -345,10 +395,11 @@ namespace MikiraSora.VirtualizingStaggeredPanel
                 var y3 = y2 - VerticalOffset;
 
                 var height = width / param.AspectRatio;
+                //钦定位置和大小
                 uiElement.Arrange(new Rect(x, y3, width, height));
             }
 
-            Debug.WriteLine($"called ArrangeOverride() done");
+            //Debug.WriteLine($"called ArrangeOverride() done");
             UpdateScrollInfo(finalSize);
             return finalSize;
         }
